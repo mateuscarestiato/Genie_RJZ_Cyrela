18cd27da2f99f9001ff6c7c460949c83daea7557
Mateus Carestiato <mateuscarestiato@users.noreply.github.com>
2026-05-08 13:18:56 -0300

    Final cleanup: remove version tag from login UI

--- auth_ui.py
+++ auth_ui.py
@@ -75,7 +75,7 @@ def render_auth_ui():
         _render_reset_password()
 
 def _render_login():
-    st.subheader("Login (v2)")
+    st.subheader("Login")
     email_prefix = st.text_input("E-mail corporativo", placeholder="seu.nome")
     password = st.text_input("Senha", type="password", autocomplete="new-password")
     
